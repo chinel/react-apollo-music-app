4ad611288f572bcdad092f9fe00be3bd4149e530
nelo <nelokwaz@gmail.com>
2021-01-20 08:21:35 +0100

    used the material ui grid to properly layout the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { AddSong, Header, SongList, SongPlayer } from "./components";
+import { Grid } from "@material-ui/core";
+
 function App() {
   return (
     <>
       <Header />
-      <AddSong />
-      <SongList />
-      <SongPlayer />
+      <Grid container space={3}>
+        <Grid item xs={12} md={7}>
+          <AddSong />
+          <SongList />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <SongPlayer />
+        </Grid>
+      </Grid>
     </>
   );
 }
