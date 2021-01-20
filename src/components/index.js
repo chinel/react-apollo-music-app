8e9180251721bab3b9166c356d2cebb4e484c78e
nelo <nelokwaz@gmail.com>
2021-01-20 08:07:23 +0100

    added all components to the component reference file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header";
+export { default as AddSong } from "./AddSong";
+export { default as SongList } from "./SongList";
+export { default as SongPlayer } from "./SongPlayer";
+export { default as QueuedSongList } from "./QueuedSongList";
