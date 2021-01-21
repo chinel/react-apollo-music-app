025a1ef649038c9eb7d9c030e4b89faff217aa88
nelo <nelokwaz@gmail.com>
2021-01-21 15:10:54 +0100

    styled the add songs component

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Button, InputAdornment, TextField } from "@material-ui/core";
+import { AddBoxOutlined, Link } from "@material-ui/icons";
 
 function AddSong() {
-  return <div>Add song</div>;
+  return (
+    <div>
+      <TextField
+        placeholder="Add Youtube or Soundcloud Url"
+        fullwidth
+        margin="normal"
+        type="url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment>
+              <Link />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button variant="contained" color="primary" endIcon={<AddBoxOutlined />}>
+        Add
+      </Button>
+    </div>
+  );
 }
 
 export default AddSong;
