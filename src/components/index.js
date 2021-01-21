77dd4f386aef2aa31544f04eb3d58143c56a880d
nelo <nelokwaz@gmail.com>
2021-01-21 16:43:35 +0100

    added the song component to the component reference file

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as AddSong } from "./AddSong";
 export { default as SongList } from "./SongList";
 export { default as SongPlayer } from "./SongPlayer";
 export { default as QueuedSongList } from "./QueuedSongList";
+export { default as Song } from "./Song";
