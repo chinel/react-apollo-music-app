f9a821a8e16d9b5c51d7348e8db5e477c04bc016
nelo <nelokwaz@gmail.com>
2021-01-21 16:34:53 +0100

    added the songlist loader icon

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { CircularProgress } from "@material-ui/core";
 
 function SongList() {
+  let loading = true;
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
   return <div>Song List</div>;
 }
 
