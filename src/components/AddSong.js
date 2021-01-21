23b6e169b8edb3c03a603e0788251339c2652750
nelo <nelokwaz@gmail.com>
2021-01-21 15:45:52 +0100

    added the dialog structure

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,11 +1,49 @@
 import React, { useState } from "react";
-import { Button, InputAdornment, TextField } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  InputAdornment,
+  TextField,
+} from "@material-ui/core";
 import { AddBoxOutlined, Link } from "@material-ui/icons";
 
 function AddSong() {
   const [dialog, setDialog] = useState(false);
+
+  function handleCloseDialog() {
+    setDialog(false);
+  }
+
   return (
     <div>
+      <Dialog open={dialog} onClose={handleCloseDialog}>
+        <DialogTitle>Edit Song</DialogTitle>
+        <DialogContent>
+          <img
+            src="https://i1.sndcdn.com/artworks-000670470790-ej1gvb-t500x500.jpg"
+            alt="song thumbnail"
+          />
+          <TextField margin="dense" name="title" label="Title" fullWidth />
+          <TextField margin="dense" name="artist" label="Artist" fullWidth />
+          <TextField
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="secondary">
+            Cancel
+          </Button>
+          <Button variant="outlined" color="primary">
+            Add Song
+          </Button>
+        </DialogActions>
+      </Dialog>
       <TextField
         placeholder="Add Youtube or Soundcloud Url"
         fullWidth
