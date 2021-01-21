d08a4657c8380e1b5a315e5a23037c74bd4cb986
nelo <nelokwaz@gmail.com>
2021-01-21 13:50:30 +0100

    styled the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { HeadsetTwoTone } from "@material-ui/icons";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <AppBar color="primary" position="fixed">
+      <Toolbar>
+        <HeadsetTwoTone />
+        <Typography variant="h6" component="h1">
+          Apollo Music Share
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
