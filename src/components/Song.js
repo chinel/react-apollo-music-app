fb0024859f7a23884b08b2c197ad50afd51ffc3f
nelo <nelokwaz@gmail.com>
2021-01-21 17:01:24 +0100

    added the structure of the song component

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,5 +1,36 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import { PlayArrow, Save } from "@material-ui/icons";
 
-export default function Song() {
-  return <div>Song</div>;
+export default function Song({ song: { thumbnail, title, artist } }) {
+  return (
+    <Card>
+      <div>
+        <CardMedia image={thumbnail} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body1" component="p" color="textSecondary">
+            {artist}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton size="small" color="primary">
+            <PlayArrow />
+          </IconButton>
+          <IconButton size="small" color="secondary">
+            <Save />
+          </IconButton>
+        </CardActions>
+      </div>
+    </Card>
+  );
 }
