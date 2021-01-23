e1caeb33668c0c9ce7b35d62b519c6c2eb2d4488
nelo <nelokwaz@gmail.com>
2021-01-23 12:59:17 +0100

    moved the sidebar to the bottom on medium screen sizes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { AddSong, Header, SongList, SongPlayer } from "./components";
-import { Grid } from "@material-ui/core";
+import { Grid, useMediaQuery } from "@material-ui/core";
 
 function App() {
+  // const matches = useMediaQuery("(min-width: 600px)"); this can also be done as shown below using the theme
+  const greaterThanMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
   return (
     <>
       <Header />
@@ -12,12 +15,20 @@ function App() {
           <SongList />
         </Grid>
         <Grid
-        style={{position: 'fixed',
-          width: '100%',
-          right: 0,
-          top: 70
-      }}
-        item xs={12} md={5}>
+          style={
+            greaterThanMd
+              ? { position: "fixed", width: "100%", right: 0, top: 70 }
+              : {
+                  position: "fixed",
+                  width: "100%",
+                  left: 0,
+                  bottom: 0,
+                }
+          }
+          item
+          xs={12}
+          md={5}
+        >
           <SongPlayer />
         </Grid>
       </Grid>
