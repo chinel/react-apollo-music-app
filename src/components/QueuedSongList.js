f52c33daf7ba1235d06df52469eb697f8151daaf
nelo <nelokwaz@gmail.com>
2021-01-22 22:25:34 -1200

    added the queue song to the the queued song list component

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import QueuedSong from "./QueuedSong";
 
 function QueuedSongList() {
   const song = {
@@ -17,7 +18,9 @@ function QueuedSongList() {
       <Typography color="textSecondary" variant="button">
         QUEUE (5)
       </Typography>
-      {Array.from({length: 5},() => song).map((song, i) => ())}
+      {Array.from({ length: 5 }, () => song).map((song, i) => (
+        <QueuedSong key={{ i }} song={song} />
+      ))}
     </div>
   );
 }
