646d9dc16fec83ef44d54b7a982356cb8b9b4f9f
nelo <nelokwaz@gmail.com>
2021-01-22 22:49:00 -1200

    created the styles for the queued song component

--- src/components/QueuedSong.js
+++ src/components/QueuedSong.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { Avatar, IconButton, Typography } from "@material-ui/core";
+import { Avatar, IconButton, Typography, makeStyles } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
 
+const useStyles = makeStyles({
+  avatar: {
+    width: 44,
+    height: 44,
+  },
+  text: {
+    textOverflow: "ellipsis",
+    overflow: "hidden",
+  },
+  container: {
+    display: "grid",
+    gridAutoFlow: "column",
+    gridTemplateColumns: "50px auto 50px",
+    gridGap: 12,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  songInfoContainer: {
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+  },
+});
+
 function QueuedSong({ song: { thumbnail, artist, title } }) {
+  const classes = useStyles();
   return (
     <div>
       <Avatar src={thumbnail} alt="Song thumbnail"></Avatar>
