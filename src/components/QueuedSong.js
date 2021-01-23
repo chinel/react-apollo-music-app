7b85bd4160331b31240e83150a89ed32566cc6e3
nelo <nelokwaz@gmail.com>
2021-01-22 22:38:01 -1200

    completed the structure of the queued song component

--- src/components/QueuedSong.js
+++ src/components/QueuedSong.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Avatar, IconButton, Typography } from "@material-ui/core";
+import { Delete } from "@material-ui/icons";
 
-function QueuedSong() {
-  return <div>Queued Song</div>;
+function QueuedSong({ song: { thumbnail, artist, title } }) {
+  return (
+    <div>
+      <Avatar src={thumbnail} alt="Song thumbnail"></Avatar>
+      <div>
+        <Typography variant="subtitle">{title}</Typography>
+        <Typography color="textSecondary" variant="body2">
+          {artist}
+        </Typography>
+      </div>
+      <IconButton>
+        <Delete color="error" />
+      </IconButton>
+    </div>
+  );
 }
 
 export default QueuedSong;
