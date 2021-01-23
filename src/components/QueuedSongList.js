8cbd92db0c06ecfaa59b3d5d7a9f8412dfa6512e
nelo <nelokwaz@gmail.com>
2021-01-22 21:39:06 -1200

    created the structure for the queued song list component

--- src/components/QueuedSongList.js
+++ src/components/QueuedSongList.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 function QueuedSongList() {
-  return <div>Queued Song List</div>;
+  return (
+    <div
+      style={{
+        margin: "10px 0",
+      }}
+    >
+      <Typography color="textSecondary" variant="button">
+        QUEUE (5)
+      </Typography>
+    </div>
+  );
 }
 
 export default QueuedSongList;
