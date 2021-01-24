e686b00f877230e95787345c3a8edcd434415dec
nelo <nelokwaz@gmail.com>
2021-01-24 12:03:48 +0100

    wrapped the app with the apollo provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import theme from "./theme";
+import { ApolloProvider } from "@apollo/client";
+import client from "./graphql/client";
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </MuiThemeProvider>,
-  document.getElementById("root")
+  <ApolloProvider client={client}>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </MuiThemeProvider>
+    , document.getElementById("root")
+  </ApolloProvider>
 );
