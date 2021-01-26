818cff05a523413f6d786a39848334494d47ba46
nelo <nelokwaz@gmail.com>
2021-01-26 14:52:18 +0100

    added the subscription graphql query

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,1 +1,13 @@
 import { gql } from "@apollo/client";
+
+export const GET_SONGS = gql`
+  subscription MyQuery {
+    songs(order_by: { created_at: desc }) {
+      url
+      thumbnail
+      id
+      duration
+      artist
+    }
+  }
+`;
