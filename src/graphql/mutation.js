8837a605f5e75e32e9ff7d2ddb609c238d9ea332
nelo <nelokwaz@gmail.com>
2021-01-25 21:59:40 -1200

    added the mutation for adding songs to the database

---
+++ src/graphql/mutation.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+const ADD_SONG = gql`
+  mutation addSongs(
+    $title: String!
+    $artist: String!
+    $thumbnail: String!
+    $duration: Float!
+    $url: String!
+  ) {
+    insert_songs(
+      objects: {
+        artist: $artist
+        duration: $duration
+        thumbnail: $thumbnail
+        title: $title
+        url: $url
+      }
+    ) {
+      affected_rows
+    }
+  }
+`;
