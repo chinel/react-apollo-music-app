aea01c70336272c0fdf666ee89cbde6db7741b3e
nelo <nelokwaz@gmail.com>
2021-01-26 14:49:13 +0100

    change the apoll client setup to accomodate subscriptions

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,45 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+/* import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://react-apollo-music.herokuapp.com/v1/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
+ */
+
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+
+const httpLink = new HttpLink({
+  uri: "https://react-apollo-music.herokuapp.com/v1/graphql", // use https for secure endpoint
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: "ws://react-apollo-music.herokuapp.com/v1/graphql", // use wss for a secure endpoint
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
+// Instantiate client
+const client = new ApolloClient({
+  link,
   cache: new InMemoryCache(),
 });
 
